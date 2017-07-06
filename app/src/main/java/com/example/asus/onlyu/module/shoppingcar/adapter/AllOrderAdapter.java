53c9f439109109b01f1ab06a31e59fc68adf64e3
Administrator <qiangdan910602@163.com>
2017-07-06 22:00:07 +0800

    强2070706晚

---
+++ app/src/main/java/com/example/asus/onlyu/module/shoppingcar/adapter/AllOrderAdapter.java
@@ -0,0 +1,46 @@
+package com.example.asus.onlyu.module.shoppingcar.adapter;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.asus.onlyu.R;
+
+/**
+ * author：郑志强
+ * data：2017/7/6 19:19
+ * function：
+ */
+
+public class AllOrderAdapter extends RecyclerView.Adapter<AllOrderAdapter.MyViewHolder>{
+    private Context mContext;
+
+    public AllOrderAdapter(Context context) {
+        mContext = context;
+    }
+
+    @Override
+    public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(mContext).inflate(R.layout.allorder_recycler_item, parent, false);
+        MyViewHolder holder=new MyViewHolder(view);
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(MyViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 4;
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+        public MyViewHolder(View itemView) {
+            super(itemView);
+        }
+    }
+}
